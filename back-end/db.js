be8f8d39cf25888916d33500e7b5d6da98f626df
sachinguptacode <sachingupta705242@gmail.com>
2025-05-13 18:25:53 +0530

    Update db.js

--- back-end/db.js
+++ back-end/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const connectdb = async () => {
   try {
-    const db = await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    const db = await mongoose.connect("mongodb://127.0.0.1:27017/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
     console.log(`db connected successfull `);
   } catch (error) {
     console.log("db not connected");
